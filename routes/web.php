5d9a48a25395d1aabb2493a6cf479d84b14e5d8a
yayanrw <yayanraw@gmail.com>
2021-11-28 23:28:57 +0700

    add router

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Http\Controllers\TodoController;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -12,6 +14,14 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('todo', 'TodoController@index');
+    $router->post('todo', 'TodoController@store');
+    $router->get('todo/{pid}', 'TodoController@show');
+    $router->put('todo/{pid}', 'TodoController@update');
+    $router->delete('todo/{pid}', 'TodoController@destroy');
+});
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
