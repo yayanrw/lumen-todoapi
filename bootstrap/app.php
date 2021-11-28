5afa56673acf03b2b67934e54c2e6cbf0b3367f9
yayanrw <yayanraw@gmail.com>
2021-11-28 23:15:52 +0700

    add lumen generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,6 +91,7 @@ $app->routeMiddleware([
 |
 */
 
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
