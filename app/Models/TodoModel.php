0e0c7d6ccbcbef8b6da8b10dcb9d80f7fdfeb785
yayanrw <yayanraw@gmail.com>
2021-11-29 06:48:02 +0700

    add table name

--- app/Models/TodoModel.php
+++ app/Models/TodoModel.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class TodoModel extends Model
 {
+    protected $table = 't_todos';
     protected $fillable = ['title', 'description', 'date', 'created_at', 'created_by', 'updated_at', 'updated_by'];
 }
